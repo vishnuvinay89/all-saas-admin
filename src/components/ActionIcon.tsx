cc21c993ab1652682ba88fcc3e9ff94e0ee1295c
Akshata Katwal <akshata.katwal@tekditechnologies.com>
2024-07-19 15:40:25 +0530

    update PR

--- src/components/ActionIcon.tsx
+++ src/components/ActionIcon.tsx
@@ -31,40 +31,6 @@ const ActionIcon: React.FC<ActionCellProps> = ({
         alignItems: "center",
       }}
     >
-      <Box
-        onClick={() => {
-          onEdit(rowData);
-        }}
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          cursor: "pointer",
-          opacity: 0.5, // Reduced opacity to make it appear disabled
-          pointerEvents: "auto", // Enable pointer events to allow click
-        }}
-      >
-        <EditIcon sx={{ color: "rgba(0, 0, 0, 0.5)" }} />
-        <Typography variant="body2">
-          Edit
-        </Typography>
-      </Box>
-      <Box
-        onClick={() => {
-          onDelete(rowData);
-        }}
-        sx={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          cursor: "pointer",
-        }}
-      >
-<DeleteIcon sx={{ color: "rgb(244, 67, 54)" }} />
-        <Typography variant="body2">
-          Delete
-        </Typography>
-      </Box>
       <Tooltip title={t("COMMON.EDIT")}>
         <Box
           onClick={() => {
@@ -95,7 +61,7 @@ const ActionIcon: React.FC<ActionCellProps> = ({
             cursor: "pointer",
           }}
         >
-          <DeleteIcon sx={{ color: "rgba(0, 0, 0, 0.5)" }} />
+<DeleteIcon sx={{ color: "rgb(244, 67, 54)" }} />
           <Typography variant="body2">Delete</Typography>
         </Box>
       </Tooltip>
