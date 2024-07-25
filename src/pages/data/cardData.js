8c7b9c9592a77789ff77628185fb93f3bb23dc25
rahul_g <rahul_g@tekditechnologies.com>
2024-07-26 02:18:21 +0530

    removed duplicate code

--- src/pages/data/cardData.js
+++ src/pages/data/cardData.js
@@ -6,8 +6,8 @@ const cardData = [
     totalBoards: 3,
     details:
       "Andhra Pradesh is a state in the southeastern coastal region of India.",
-    boards: ["NIOS", "State", "ICSE"],
-    subjects: ["English", "Maths", "Science"],
+    boards: ["AP Board", "NIOS", "ICSE"],
+    subjects: ["Telugu", "English", "Maths"],
   },
   {
     id: "2",
@@ -16,8 +16,8 @@ const cardData = [
     totalBoards: 3,
     details:
       "Maharashtra is a state in the western peninsular region of India.",
-    boards: ["NIOS", "State", "ICSE"],
-    subjects: ["English", "Maths", "Science"],
+    boards: ["Maharashtra Board", "CBSE", "ICSE"],
+    subjects: ["Marathi", "English", "Science"],
   },
   {
     id: "3",
@@ -25,8 +25,8 @@ const cardData = [
     boardsUploaded: 3,
     totalBoards: 3,
     details: "Karnataka is a state in the southwestern region of India.",
-    boards: ["NIOS", "State", "ICSE"],
-    subjects: ["English", "Maths", "Science"],
+    boards: ["Karnataka Board", "ICSE", "NIOS"],
+    subjects: ["Kannada", "Maths", "Science"],
   },
   {
     id: "4",
@@ -34,8 +34,8 @@ const cardData = [
     boardsUploaded: 1,
     totalBoards: 3,
     details: "Tamil Nadu is a state in the southern part of India.",
-    boards: ["NIOS", "State", "ICSE"],
-    subjects: ["English", "Maths", "Science"],
+    boards: ["Tamil Nadu Board", "NIOS", "CBSE"],
+    subjects: ["Tamil", "English", "Social Studies"],
   },
   {
     id: "5",
@@ -43,8 +43,8 @@ const cardData = [
     boardsUploaded: 2,
     totalBoards: 3,
     details: "Kerala is a state on the southwestern Malabar Coast of India.",
-    boards: ["NIOS", "State", "ICSE"],
-    subjects: ["English", "Maths", "Science"],
+    boards: ["Kerala Board", "ICSE", "CBSE"],
+    subjects: ["Malayalam", "Science", "Maths"],
   },
   {
     id: "6",
@@ -52,8 +52,8 @@ const cardData = [
     boardsUploaded: 3,
     totalBoards: 3,
     details: "Telangana is a state in the south-central region of India.",
-    boards: ["NIOS", "State", "ICSE"],
-    subjects: ["English", "Maths", "Science"],
+    boards: ["Telangana Board", "NIOS", "ICSE"],
+    subjects: ["Telugu", "Science", "Social Studies"],
   },
   {
     id: "7",
@@ -61,8 +61,8 @@ const cardData = [
     boardsUploaded: 1,
     totalBoards: 3,
     details: "Gujarat is a state on the western coast of India.",
-    boards: ["NIOS", "State", "ICSE"],
-    subjects: ["English", "Maths", "Science"],
+    boards: ["Gujarat Board", "CBSE", "ICSE"],
+    subjects: ["Gujarati", "Maths", "Science"],
   },
   {
     id: "8",
@@ -70,8 +70,8 @@ const cardData = [
     boardsUploaded: 2,
     totalBoards: 3,
     details: "Rajasthan is a state in northern India.",
-    boards: ["NIOS", "State", "ICSE"],
-    subjects: ["English", "Maths", "Science"],
+    boards: ["Rajasthan Board", "ICSE", "NIOS"],
+    subjects: ["Hindi", "English", "Maths"],
   },
   {
     id: "9",
@@ -79,8 +79,8 @@ const cardData = [
     boardsUploaded: 3,
     totalBoards: 3,
     details: "Madhya Pradesh is a state in central India.",
-    boards: ["NIOS", "State", "ICSE"],
-    subjects: ["English", "Maths", "Science"],
+    boards: ["MP Board", "CBSE", "NIOS"],
+    subjects: ["Hindi", "Maths", "Science"],
   },
   {
     id: "10",
@@ -88,8 +88,8 @@ const cardData = [
     boardsUploaded: 1,
     totalBoards: 3,
     details: "Uttar Pradesh is a state in northern India.",
-    boards: ["NIOS", "State", "ICSE"],
-    subjects: ["English", "Maths", "Science"],
+    boards: ["UP Board", "ICSE", "CBSE"],
+    subjects: ["Hindi", "Science", "Social Studies"],
   },
 ];
 
