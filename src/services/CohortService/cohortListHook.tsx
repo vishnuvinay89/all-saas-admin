05fc5ac0cd20ec0f1115d9af8f8dfbc6fd169286
rahul_g <rahul_g@tekditechnologies.com>
2024-07-31 11:00:10 +0530

    resolved type issue

--- src/services/CohortService/cohortListHook.tsx
+++ src/services/CohortService/cohortListHook.tsx
@@ -1,10 +1,10 @@
 import { useQuery } from "@tanstack/react-query";
-import { getCohortList } from "./cohortService";
+import { getCohortList, cohortListData } from "./cohortService";
 
 export const refetchInterval: number = 5 * 60 * 1000;
 export const gcTime: number = 10 * 60 * 1000;
 
-export const useCohortList = (userId: string) => {
+export const useCohortList = (userId: cohortListData) => {
   return useQuery({
     queryKey: ["cohort"],
     queryFn: () => getCohortList(userId),
