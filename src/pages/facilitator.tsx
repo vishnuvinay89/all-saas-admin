a01ae1083e34ef680f48b4a739170e63f3da2ccf
vivek kasture <vivek_kasture@techjoomla.com>
2024-08-10 18:22:09 +0530

    Issue #PS-000 fix: Resolved UT issues

---
+++ src/pages/facilitator.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import UserTable from "@/components/UserTable";
+import { useTranslation } from "next-i18next";
+import { Role, FormContextType } from "@/utils/app.constant";
+import CommonUserModal from "@/components/CommonUserModal";
+import useSubmittedButtonStore from "@/utils/useSharedState";
+
+const Facilitator: React.FC = () => {
+  const { t } = useTranslation();
+  const [openAddFacilitatorModal, setOpenAddFacilitatorModal] =
+    React.useState(false);
+  const [submitValue, setSubmitValue] = React.useState<boolean>(false);
+  const setSubmittedButtonStatus = useSubmittedButtonStore((state:any) => state.setSubmittedButtonStatus);
+
+  const handleOpenAddFacilitatorModal = () => {
+    setOpenAddFacilitatorModal(true);
+  };
+  const handleModalSubmit = (value: boolean) => {
+    setSubmitValue(true);
+  };
+  const handleCloseAddFacilitatorModal = () => {
+    setSubmittedButtonStatus(false)
+    setOpenAddFacilitatorModal(false);
+  };
+
+  const handleAddFaciliatorClick = () => {
+    handleOpenAddFacilitatorModal();
+  };
+  return (
+    <>
+      <UserTable
+        role={Role.TEACHER}
+        userType={t("SIDEBAR.FACILITATORS")}
+        searchPlaceholder={t("FACILITATORS.SEARCHBAR_PLACEHOLDER")}
+        handleAddUserClick={handleAddFaciliatorClick}
+        parentState={submitValue}
+      />
+      <CommonUserModal
+        open={openAddFacilitatorModal}
+        onClose={handleCloseAddFacilitatorModal}
+        userType={FormContextType.TEACHER}
+        onSubmit={handleModalSubmit}
+      />
+    </>
+  );
+};
+
+export async function getStaticProps({ locale }: any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
+}
+
+export default Facilitator;
---
+++ src/pages/team-leader.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import UserTable from "@/components/UserTable";
+import { useTranslation } from "next-i18next";
+import { Role, FormContextType } from "@/utils/app.constant";
+import CommonUserModal from "@/components/CommonUserModal";
+const TeamLeader: React.FC = () => {
+  const { t } = useTranslation();
+  const handleAddTeamLeaderClick = () => {
+    handleOpenAddTeamLeaderModal();
+  };
+  const [submitValue, setSubmitValue] = React.useState<boolean>(false);
+
+  const [openAddTeamLeaderModal, setOpenAddTeamLeaderModal] =
+    React.useState(false);
+  const handleOpenAddTeamLeaderModal = () => {
+    setOpenAddTeamLeaderModal(true);
+  };
+  const handleModalSubmit = (value: boolean) => {
+    setSubmitValue(true);
+  };
+  const handleCloseAddTeamLeaderModal = () => {
+    setOpenAddTeamLeaderModal(false);
+  };
+
+  return (
+    <>
+      <UserTable
+        role={Role.TEAM_LEADER}
+        searchPlaceholder={t("TEAM_LEADERS.SEARCHBAR_PLACEHOLDER")}
+        userType={t("SIDEBAR.TEAM_LEADERS")}
+        handleAddUserClick={handleAddTeamLeaderClick}
+        parentState={submitValue}
+      />
+
+      <CommonUserModal
+        open={openAddTeamLeaderModal}
+        onClose={handleCloseAddTeamLeaderModal}
+        onSubmit={handleModalSubmit}
+        userType={FormContextType.TEAM_LEADER}
+      />
+    </>
+  );
+};
+
+export async function getStaticProps({ locale }: any) {
+  return {
+    props: {
+      ...(await serverSideTranslations(locale, ["common"])),
+    },
+  };
+}
+
+export default TeamLeader;
