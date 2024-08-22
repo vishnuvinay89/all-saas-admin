9fd377f93485f7277705faeaae69f1b8b420d30d
Akshata Katwal <akshata.katwal@tekditechnologies.com>
2024-08-22 22:48:22 +0530

    add previous navbar changes

--- src/components/layouts/FullLayout.tsx
+++ src/components/layouts/FullLayout.tsx
@@ -136,8 +136,8 @@ const FullLayout = ({ children }: any) => {
       <Header
         sx={{
           paddingLeft: isSidebarOpen && lgUp ? "265px" : "",
-          backgroundColor: "#FFFFFF",
-          boxshow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+          backgroundColor: "#4d4639",
+                    boxshow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
         }}
         toggleMobileSidebar={() => setMobileSidebarOpen(true)}
       />
