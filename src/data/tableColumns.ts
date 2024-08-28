ba547159c5ad9989f5e4edd2c4aafc788b08a0bb
upendraTekdi <upendra_k@tekditechnologies.com>
2024-08-28 16:40:36 +0530

    Task #225623: change name of active and archieve members to learners

--- src/data/tableColumns.ts
+++ src/data/tableColumns.ts
@@ -68,10 +68,14 @@ export const getCenterTableData = (t: any, isMobile: boolean) => {
     { key: "createdAt", titleKey: "TABLE_TITLE.CREATED_DATE", width: 130 },
     { key: "updatedAt", titleKey: "TABLE_TITLE.UPDATED_DATE", width: 130 },
 
-    { key: "totalActiveMembers", titleKey: "Active Members", width: 130 },
+    {
+      key: "totalActiveMembers",
+      titleKey: "CENTERS.ACTIVE_LEARNERS",
+      width: 130,
+    },
     {
       key: "totalArchivedMembers",
-      titleKey: "Archived Members",
+      titleKey: "CENTERS.ARCHIVED_LEARNERS",
       width: 130,
     },
     { key: "actions", titleKey: "TABLE_TITLE.ACTIONS", width: 125 },
